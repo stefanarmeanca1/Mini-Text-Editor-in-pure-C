1f52f24334d301cce643b2164dd608312faf701e
stefanarmeanca1 <stefanarmeanca1@gmail.com>
2026-06-20 21:42:14 +0300

    First commit

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/*
+Vreau o aplicație în consolă unde pot:
+
+1.Deschide un fișier text
+-dacă există, îl încarcă în memorie;
+-dacă nu există, pornește cu document gol.
+
+2.Edita textul
+-inserare caracter;
+-ștergere caracter;
+-inserare linie nouă;
+-mutare cursor stânga/dreapta/sus/jos.
+
+3.Salva fișierul
+-scriere pe disc;
+-detectare dacă documentul a fost modificat.
+
+4.Căutare în text
+-caut un cuvânt;
+-îmi arată prima apariție;
+-apoi pot continua căutarea.
+
+5.Undo / Redo
+-pot anula ultima acțiune;
+-pot reface acțiunea anulată.
+
+6.Comenzi simple
+- :w salvează;
+- :q iese;
+- :wq salvează și iese;
+- :find ceva caută text;
+- :help afișează comenzile.
+
+7.Status bar
+-nume fișier;
+-linie/coloană curentă;
+-dacă fișierul este modificat.
+*/
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
